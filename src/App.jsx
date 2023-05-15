eec00c99be50a59c27361d4526998f2c6f6deed5
mustafa <mustafanaiby@gmail.com>
2023-05-15 11:25:13 +0430

    q

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="header">Header</div>
+      <div className="header"></div>
       <div className="content-large">content1</div>
-      <div className="content-large">content2</div>
-      <div className="content-large">content2</div>
+      <div className="content-small">content2</div>
+      <div className="content-small">content2</div>
+      <div className="footer">footer</div>
     </div>
   );
 }
