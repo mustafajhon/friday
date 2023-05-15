cfdfd642667beb24bbb85812520a24d55c35460a
mustafa <mustafanaiby@gmail.com>
2023-05-15 12:38:57 +0430

    y

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <h4>hello world</h4>
+    </div>
+  );
 }
 
 export default App;
