e906d0c7e209d6514b2d4cac3db5675f8d5db899
mustafa <mustafanaiby@gmail.com>
2023-05-15 12:36:36 +0430

    d

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="header"></div>
-      <div className="content-large">content1</div>
-      <div className="content-small">content2</div>
-      <div className="content-small">content2</div>
-      <div className="footer">footer</div>
-    </div>
-  );
+  return <div className="container"></div>;
 }
 
 export default App;
