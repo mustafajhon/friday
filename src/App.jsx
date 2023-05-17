cc5b8332bfd94c0e2a0175811ca34cb40eed304f
mustafa <mustafanaiby@gmail.com>
2023-05-17 11:03:15 +0430

    s

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,20 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <h4>hello world</h4>
+      <div className="item">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exer Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        praesentium ullam numquam, dolore nobis quo suscipit impedit quasi.
+        Veniam, eum.
+      </div>
+      <div className="item">item 2</div>
+      <div className="item">item 3</div>
+      <div className="item">item 4</div>
+      <div className="item">item 5</div>
+      <div className="item">item 6</div>
+      <div className="item">item 7</div>
+      <div className="item">item 8</div>
+      <div className="item">item 9</div>
     </div>
   );
 }
