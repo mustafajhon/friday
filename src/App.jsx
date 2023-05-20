7757ebb34f5d8bfd464f43b572cddb91bcbe2250
mustafa <mustafanaiby@gmail.com>
2023-05-20 11:32:59 +0430

    w

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ function App() {
   return (
     <div>
       <div className="container">
-        <h2>hello world</h2>
+        <div className="item">items 1</div>
+        <div className="item">items 2</div>
+        <div className="item">items 3</div>
+        <div className="item">items 4</div>
+        <div className="item">items 5</div>
+        <div className="item">items 6</div>
+        <div className="item">items 7</div>
+        <div className="item">items 8</div>
       </div>
     </div>
   );
